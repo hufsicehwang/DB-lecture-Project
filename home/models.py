561bcf7c08cd61de73c37dd658a428917a46971b
junyoung.kim <48875061+wnsdudSoftkim@users.noreply.github.com>
2021-12-04 15:18:47 +0900

    ㅁ

--- home/models.py
+++ home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class MovieList(models.Model):
+    id = models.AutoField(primary_key=True)
+    movieNmEn = models.TextField(verbose_name="movieNmEn")#제목
+    genreAlt = models.TextField(verbose_name="genreAlt")#장르
+    predtYear = models.TextField(verbose_name="predtYear")#개봉일
+    nationAlt = models.TextField(verbose_name="nationAlt")#개봉나라
+    
+    
